e496b3bf3caab2b6d81da4f8b8a14f9fc49f6524
stormcat24 <stormcat24@stormcat.io>
2018-05-25 02:47:21 +0900

    refactoring

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,25 @@
 FROM mysql:5.7
 
+# (1) パッケージアップデートとwgetインストール
 RUN apt-get update
-RUN apt-get install -y wget telnet
+RUN apt-get install -y wget
+
+# (2) entrykitのインストール
 RUN wget https://github.com/progrium/entrykit/releases/download/v0.4.0/entrykit_0.4.0_linux_x86_64.tgz
 RUN tar -xvzf entrykit_0.4.0_linux_x86_64.tgz
 RUN rm entrykit_0.4.0_linux_x86_64.tgz
 RUN mv entrykit /usr/local/bin/
 RUN entrykit --symlink
 
+# (3) スクリプトと各種設定ファイルのコピー
 COPY add-server-id.sh /usr/local/bin/
-COPY init-data.sh /usr/local/bin/
 COPY etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/
 COPY etc/mysql/conf.d/mysql.cnf /etc/mysql/conf.d/
 COPY prepare.sh /docker-entrypoint-initdb.d
+COPY init-data.sh /usr/local/bin/
 COPY sql /sql
 
+# (4) スクリプトとmysqldの実行
 ENTRYPOINT [ \
   "prehook", \
     "add-server-id.sh", \
--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
+# (1) MasterとSlaveを環境変数で制御する
 if [ ! -z "$MYSQL_MASTER" ]; then
   echo "this container is master"
   return 0
@@ -7,10 +8,15 @@ fi
 
 echo "prepare as slave"
 
-# health check to master
+# (2) SlaveからMasterへの疎通確認をする
+if [ -z "$MYSQL_MASTER_HOST" ]; then
+  echo "mysql_master_host is not specified" 1>&2
+  return 1
+fi
+
 while :
 do
-  if mysql -h master -u root -p$MYSQL_ROOT_PASSWORD -e "quit" > /dev/null 2>&1 ; then
+  if mysql -h $MYSQL_MASTER_HOST -u root -p$MYSQL_ROOT_PASSWORD -e "quit" > /dev/null 2>&1 ; then
     echo "MySQL master is ready!"
     break
   else
@@ -19,27 +25,20 @@ do
   sleep 3
 done
 
-# check existance of tables in master database
-if mysql -h master -u root -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE -e "show tables;" | wc -l ; then
-  MASTER_DATA_EMPTY=true
-else
-  MASTER_DATA_EMPTY=false
-fi
+# (3) Masterにレプリケーション用のユーザーと権限の作成
+mysql -h $MYSQL_MASTER_HOST -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER '$MYSQL_REPL_USER'@'`hostname -i`' IDENTIFIED BY '$MYSQL_REPL_PASSWORD';"
+mysql -h $MYSQL_MASTER_HOST -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT REPLICATION SLAVE ON *.* TO '$MYSQL_REPL_USER'@'`hostname -i`';"
 
-# create replication user
-mysql -h master -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER 'repl'@'`hostname -i`' IDENTIFIED BY '$MYSQL_REPL_PASSWORD';"
-mysql -h master -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT REPLICATION SLAVE ON *.* TO '$MYSQL_REPL_USER'@'`hostname -i`';"
-
-# get master status
+# (4) Masterのbinlogのポジションを取得
 MASTER_STATUS_FILE=/tmp/master-status
-mysql -h master -u root -p$MYSQL_ROOT_PASSWORD -e "SHOW MASTER STATUS\G" > $MASTER_STATUS_FILE
+mysql -h $MYSQL_MASTER_HOST -u root -p$MYSQL_ROOT_PASSWORD -e "SHOW MASTER STATUS\G" > $MASTER_STATUS_FILE
 BINLOG_FILE=`cat $MASTER_STATUS_FILE | grep File | xargs | cut -d' ' -f2`
 BINLOG_POSITION=`cat $MASTER_STATUS_FILE | grep Position | xargs | cut -d' ' -f2`
 echo "BINLOG_FILE=$BINLOG_FILE"
 echo "BINLOG_POSITION=$BINLOG_POSITION"
 
-# start replication
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CHANGE MASTER TO MASTER_HOST='master', MASTER_USER='$MYSQL_REPL_USER', MASTER_PASSWORD='$MYSQL_REPL_PASSWORD', MASTER_LOG_FILE='$BINLOG_FILE', MASTER_LOG_POS=$BINLOG_POSITION;"
+# (5) レプリケーションを開始する 
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CHANGE MASTER TO MASTER_HOST='$MYSQL_MASTER_HOST', MASTER_USER='$MYSQL_REPL_USER', MASTER_PASSWORD='$MYSQL_REPL_PASSWORD', MASTER_LOG_FILE='$BINLOG_FILE', MASTER_LOG_POS=$BINLOG_POSITION;"
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "START SLAVE;"
 
 echo "slave started"
